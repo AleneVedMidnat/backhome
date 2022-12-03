5731e6f1181aaed6348de9e9d318dca55aca65db
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2022-12-03 13:58:33 +0000

    working portal

--- back home/Assets/Scripts/portal stone/PortalStoneController.cs
+++ back home/Assets/Scripts/portal stone/PortalStoneController.cs
@@ -6,13 +6,24 @@ public class PortalStoneController : MonoBehaviour
 {
     int portalStonesOn = 0;
     bool portalOpen = false;
+    [SerializeField] Sprite portalOnSprite;
+    [SerializeField] GameObject m_leaveUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (portalOpen == true)
+        {
+            m_leaveUI.SetActive(true);
+        }
+    }
 
     public void AddPortalStone()
     {
         portalStonesOn++;
-        if (portalStonesOn == 5)
+        if (portalStonesOn >= 4)
         {
             portalOpen = true;
+            gameObject.GetComponent<SpriteRenderer>().sprite = portalOnSprite;
             //need to enable an object here
         }
     }

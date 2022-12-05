da7ba2f64f0e382b0a3565a40ab601d15d730b97
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2022-12-05 15:22:57 +0000

    ui to menu fix

--- back home/Assets/UI/pauseUI.cs
+++ back home/Assets/UI/pauseUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pauseUI : MonoBehaviour
 {
@@ -26,7 +27,7 @@ public class pauseUI : MonoBehaviour
 
     public void ToMenu()
     {
-        //scene manager to menu
+        SceneManager.LoadScene("Start");
     }
 
      public void ToContinue()
